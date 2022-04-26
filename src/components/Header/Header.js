b6f2bea2e105691ae938fe675a245d6e82416c5b
Guillaume Sere <guillaumesere@hotmail.fr>
2022-04-26 11:03:42 +0200

    create error page and header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Logo from '../../assets/argentBankLogo.png';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+
+/**show Header  
+ * @returns {JSX}
+ */
+
+export default function Header(){
+
+  return ( 
+    <>
+      <header className='header'>
+        <nav className="main-nav">
+          <Link className="main-nav-logo" to="/">
+            <img className="main-nav-logo-image" src={Logo} alt="Argent Bank Logo"/>
+            <h1 className="sr-only">Argent Bank</h1>
+          </Link>
+          <div>
+            <Link to="/profile" className='main-nav-item'>
+            <i className="fa fa-user-circle"></i>
+            Sign in
+            </Link>
+            <Link className="main-nav-item" to="/login">
+            <i className='fa fa-sign-out'></i>
+            Sign Out
+            </Link>
+          </div>
+        </nav>
+      </header>   
+    </>     
+  );  
+}
+
---
+++ src/pages/Error.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Error.css';
+import { Link } from 'react-router-dom';
+
+
+/**show Page Error
+ *  
+ * @returns {JSX}
+ */
+
+ export default function Error(){
+    
+    return(
+        <>
+            <main className='error-main'>
+                <div className='error-container'>
+                    <p className='error-text-number'>404</p>
+                    <p className='error-text'>Whoops! The page you requested does not exist.</p>
+                    <Link to='/' className='error-btn'>Return to the homepage</Link>
+                </div>
+            </main>    
+        </>
+    )
+}
